7a0b22c532c06628a3e40ab80920555744a42911
Landon McKell <landon.roney7923@gmail.com>
2020-07-27 08:54:19 -0600

    most of the backend

--- config/db.js
+++ config/db.js
@@ -6,7 +6,12 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db,  {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     console.log("MongoDB Connected...");
   } catch (err) {
     console.log(err.message);
